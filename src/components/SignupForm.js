491b979804ef47877f02df2d897899f078a50109
gem-ananyaverma <ananya.verma@geminisolutions.in>
2021-07-16 16:27:46 +0530

    regex improved for email

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -22,7 +22,7 @@ const validate = values=>{
     }
     if(!values.email){
         errors.email='Required'
-    }else if (!/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/.test(values.email)){
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
         errors.email='Invalid email'
     }
     if(!values.phone){
