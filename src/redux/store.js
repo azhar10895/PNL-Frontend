3be3d132d582c0b11f0c0f2c20ce76023c5a42f0
gem-ananyaverma <ananya.verma@geminisolutions.in>
2021-07-26 15:19:37 +0530

    store created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,10 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import fetchReducer from './Reducers/fetchReducer'
 
+
+
+const store = createStore(fetchReducer, applyMiddleware(thunk))
+  
+export default store;
+  
