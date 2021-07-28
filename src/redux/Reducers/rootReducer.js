775a447b14facf48a9fb782c2b17e2c4a43f47db
Manav Maheshwari <manav.maheshwari@geminisolutions.in>
2021-07-28 19:58:13 +0530

    Completed integration of merge function in redux

--- src/redux/Reducers/rootReducer.js
+++ src/redux/Reducers/rootReducer.js
@@ -1,24 +1,29 @@
-import * as types from '../types/rootReducerType';
+import * as types from "../types/rootReducerType";
 
 const initialState = null;
-const mergeData=(state,payload)=>{
-    
-}
+const mergeData = (state, payload) => {
+  const accounts_new = Object.keys(payload);
+  accounts_new.map((accountNo) => {
+    state.pnlData[accountNo].data = { ...state.pnlData[accountNo].data };  //changing state data array to object
+    payload[accountNo].data = { ...payload[accountNo].data };  //changing payload data array to object
+    state.pnlData[accountNo] = { ...state.pnlData[accountNo], ...payload[accountNo] }; //merging
+    state.pnlData[accountNo].data = Object.values(state.pnlData[accountNo].data); // changing data object to array
+  });
+};
 
-const rootReducer = (state= initialState,{type, payload})=>{
-
-    switch(type){
-        case types.FETCH_API:{
-            const updatedState = payload;
-            return {...updatedState};
-        }
-        /* case types.MERGE_API:{
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case types.FETCH_API: {
+      const updatedState = payload;
+      return { ...updatedState };
+    }
+    /* case types.MERGE_API:{
             const mergeState=
             return mergeState;
         }; */
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
