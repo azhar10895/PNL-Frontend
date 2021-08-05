67916aabcf3d440a04030591848084b0a02e2197
dwivedipalak <palak.dwivedi@geminisolutions.in>
2021-08-05 20:42:18 +0530

    getBrokeragerate API called

--- src/components/Trades/settings/index.js
+++ src/components/Trades/settings/index.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { getApiCall } from "../../../utils/axios";
+import { API_URLS } from "../../../config/index";
 
- const Settings = () => {
-    return (
-        <div>
-            <input placeholder="Enter rate" />
-        </div>
-    )
-}
+const Settings = () => {
+  const [data, setData] = useState([]);
+  const getBrokerage = async () => {
+    try {
+      const res = await getApiCall(API_URLS.getBrokerage, {});
+      const data = res?.res;
+      console.log("res:::::::::",res);
+      
+    } catch (err) {}
+  };
+  return (
+    <div>
+      <input placeholder="Enter rate" />
+    </div>
+  );
+};
+
 export default Settings;
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 export const API_URLS = {
-  baseUrl: "http://3.108.174.21:3000/",
-  getPNL: "trades/get-pnl",
-  getTrades: "trades/get-trades",
-  login: "authentication/login",
-  getAccounts: "trades/get-accounts"
+    baseUrl: "http://3.108.174.21:3000/",
+    getPNL: "trades/get-pnl",
+    getTrades: "trades/get-trades",
+    login: "authentication/login",
+    getAccounts: "trades/get-accounts",
+    getBrokerage: "trades/get-brokerage",
+
 };
