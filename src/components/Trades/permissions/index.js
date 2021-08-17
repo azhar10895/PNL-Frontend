c719d06f63078aa2439f248287a0252108a33da0
dwivedipalak <palak.dwivedi@geminisolutions.in>
2021-08-17 14:26:42 +0530

    permissions

--- src/components/Trades/permissions/index.js
+++ src/components/Trades/permissions/index.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { getApiCall, postApiCall } from "../../../utils/axios";
 import { API_URLS } from "../../../config/index";
 import Select from "react-select";
-import "../styles/settings.css";
+import "../styles/permissions.css";
 import NavigationEveryPage from "../../Nav/NavigationEveryPage";
-import { useFormik } from "formik";
 import { Redirect } from "react-router";
 
- const Permissions = () => {
-    return (
-        <>
-        <div className="PermissionsPage container">
-        <div>
-            <NavigationEveryPage pageName="Permissions" />
-          </div>
-        <div>
-            <input placeholder=" new page created" />
+const Permissions = () => {
+  return (
+    <>
+      <div className="PermissionsPage">
+        <div className="PermissionsBar">
+          <NavigationEveryPage pageName="Permissions" />
         </div>
+        <div>
+          <input placeholder=" new page created" />
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Permissions;
