d16963772c69f4fc33ffb5dcbdea21b559434569
dwivedipalak <palak.dwivedi@geminisolutions.in>
2021-09-03 16:09:09 +0530

    edit pencil icon

--- src/components/authentication/permissions/Users.js
+++ src/components/authentication/permissions/Users.js
@@ -61,16 +61,30 @@ const Users = () => {
                   </div>
                   <div className="col-2">
                     <div className="float-end">
-                      <button
+                      
+                      {/* <button
                         type="button"
                         className="BrokerageSubmitButton"
                         value={data[user]?.userName}
                         name={data[user]?.userName}
                         id={user}
                         onClick={editHandler}
+                        
                       >
-                        Edit
-                      </button>
+                        Edit */}
+                        {/* <Icon.PencilFill color="white" size={30} /> */}
+                      {/* </button> */}
+                      
+                      {/* <svg xmlns="http://www.w3.org/2000/svg" value={data[user]?.userName}
+                        name={data[user]?.userName}
+                        id={user}
+                        onClick={editHandler} width="32" height="32" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg> */}
+                      <svg xmlns="http://www.w3.org/2000/svg" name={data[user]?.userName}
+                        id={user}
+                        onClick={editHandler} width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+</svg>
+                      
                     </div>
                   </div>
                 </div>
